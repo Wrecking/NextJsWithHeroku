5ea8c6aedd84668fd3c4b152a3b95ff5b9be3b05
Wrecking <Wrecking@users.noreply.github.com>
2020-10-12 14:51:52 +0300

    Update firebase-config.js

--- config/firebase-config.js
+++ config/firebase-config.js
@@ -1,13 +1,13 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCK6waskCLjn5Kmsr0n7W46zzwFOc_w3_U",
-  authDomain: "nextjs-app-5a251.firebaseapp.com",
-  databaseURL: "https://nextjs-app-5a251.firebaseio.com",
-  projectId: "nextjs-app-5a251",
-  storageBucket: "nextjs-app-5a251.appspot.com",
-  messagingSenderId: "357458582070",
-  appId: "1:357458582070:web:189254d3c6d11f5a525697",
+  apiKey: "",
+  authDomain: "",
+  databaseURL: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 
 try {
